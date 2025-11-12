d00dff5a968afb8f10bcf7658f2d37309620fc32
MartinYiWu <756589808@qq.com>
2025-11-12 16:33:51 +0800

    基础项目结构提交

--- designer-toys/designer-toys-gateway-service/src/main/java/com/jjcy/arttoy/gateway/GatewayApplication.java
+++ designer-toys/designer-toys-gateway-service/src/main/java/com/jjcy/arttoy/gateway/GatewayApplication.java
@@ -3,10 +3,12 @@ package com.jjcy.arttoy.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 网关服务启动类
  */
+@EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient  // 启用服务发现，注册到Nacos
 public class GatewayApplication {
--- designer-toys/designer-toys-gateway-service/src/main/java/com/jjcy/arttoy/gateway/config/GatewayConfig.java
+++ designer-toys/designer-toys-gateway-service/src/main/java/com/jjcy/arttoy/gateway/config/GatewayConfig.java
@@ -2,6 +2,7 @@ package com.jjcy.arttoy.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
