800e6b5d7d26a3bfb6d618a6355cabd22cedde1e
MartinYiWu <756589808@qq.com>
2025-11-18 15:58:31 +0800

    加入部分代码

--- designer-toys/designer-toys-user-service/designer-toys-userservice/src/main/java/com/designer/toys/user/controller/UserController.java
+++ designer-toys/designer-toys-user-service/designer-toys-userservice/src/main/java/com/designer/toys/user/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.designer.toys.user.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping("/v1/user")
 public class UserController {
+    @GetMapping("/getUser")
+    public String getUser() {
+        return "hello world";
+    }
 }
